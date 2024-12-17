87d911e1a7a335e769ab4d6185db7e0b23a023e5
sumin305 <gogotnals@naver.com>
2024-12-17 11:29:33 +0900

    useScroll로 스크롤 현황 나타내기

---
+++ animation/src/app/use-scroll/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useScroll, animated, useSpring } from "@react-spring/web";
+
+const Scroll: React.FC = () => {
+  const { scrollX, scrollXProgress, scrollY, scrollYProgress } = useScroll({
+    onChange: ({ value: { scrollYProgress } }) => {
+      api.start({ width: scrollYProgress * 100 + "%" });
+    },
+  });
+  const [props, api] = useSpring(() => {
+    {
+      width: "0";
+    }
+  });
+
+  // scrollYProgress
+  // 0: 스크롤이 움직이지 않은 상태
+  // 1: 스크롤이 최대로 움직인 상태
+
+  // scrollY
+  // 스크롤이 실제로 움직인 거리
+  return (
+    <div className="w-full h-[2000px] flex justify-center items-center">
+      <animated.div
+        style={{ ...props, width: scrollYProgress.to((v) => `${v * 100}%`) }}
+        className="h-5 bg-pink-400 fixed top-0 left-0"
+      ></animated.div>
+      <div className="flex flex-col space-y-6 fixed top-5">
+        <animated.span>{scrollY.to((v) => v.toFixed(0))}</animated.span>
+        <animated.span>{scrollYProgress.to((v) => v.toFixed(2))}</animated.span>
+      </div>
+    </div>
+  );
+};
+
+export default Scroll;
